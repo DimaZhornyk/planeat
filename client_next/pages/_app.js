5577c478be8f0c826aec83d1449f5618601f8ac9
Dmytro Zhornyk <dima.zhornyk@gmail.com>
2020-10-25 17:43:38 +0200

    added fonts

--- client_next/pages/_app.js
+++ client_next/pages/_app.js
@@ -4,6 +4,7 @@ import {useStore} from '../store'
 import {applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk"
 import '../styles/globals.css'
+import Head from "next/head"
 
 export default function App({Component, pageProps}) {
 
@@ -11,6 +12,9 @@ export default function App({Component, pageProps}) {
 
     return (
         <Provider store={store}>
+            <Head>
+                <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap&subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet"/>
+            </Head>
             <Component {...pageProps} />
         </Provider>
     )
