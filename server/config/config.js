74e016ee3f60dc812cad1e490cbc8e254da1ed55
Dmytro Zhornyk <dmytro.zhornyk@itrexgroup.com>
2021-06-01 23:26:07 +0300

    rename auth service, create common entrypoint

--- server/config/config.js
+++
@@ -1,6 +0,0 @@
-function getMongoURI(username, password) {
-    console.log(`mongodb+srv://${username}:${password}@planeat.f1hnx.mongodb.net`)
-    return `mongodb+srv://${username}:${password}@planeat.f1hnx.mongodb.net`
-}
-
-export default getMongoURI
